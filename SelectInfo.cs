696708942633fc7d23e5b7422e5f4227d8439f9c
pr@patagames.com <pr@patagames.com>
2015-06-13 21:16:10 +1000

    Initial commit

--- SelectInfo.cs
+++ SelectInfo.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-namespace Patagames.Pdf.Net.Controls.WinForms
+﻿namespace Patagames.Pdf.Net.Controls.WinForms
 {
 	/// <summary>
 	/// Represents the information about the selected text in the control
